e53adf0c6c66ece156f9b548fd9d7ed0277c99aa
JinHwan Hwang <calanchue@naver.com>
2013-03-20 15:31:02 +0900

    tab is modified to 4 space. pep default

--- inout_manager/views.py
+++ inout_manager/views.py
@@ -6,16 +6,16 @@ from django.shortcuts import render_to_response
 from inout_manager.models import *
 
 def index(request):
-  return render_to_response('starter-template.html',{}, context_instance=RequestContext(request))
+    return render_to_response('starter-template.html',{}, context_instance=RequestContext(request))
 
 def view(request):
-  return HttpResponse("ok");
+    return HttpResponse("ok");
 
 def bootstrap_test(request):
-  return render_to_response('inout_manager/index.html',{}, context_instance=RequestContext(request))
+    return render_to_response('inout_manager/index.html',{}, context_instance=RequestContext(request))
 
 def response(request):
-  #latest_player = Player.objects.order_by('name')[:5]
+    #latest_player = Player.objects.order_by('name')[:5]
     #template = loader.get_template('inout_manager/index.html')
     #context = Context({'latest_poll_list': latest_player,})
     #return HttpResponse(template.render(context))
@@ -23,34 +23,34 @@ def response(request):
     return render(request, 'inout_manager/index.html', context)
 
 class MigHistory:
-  def __init__(self, prev_event, recent_event):
-    """ recent event must not be none """
+    def __init__(self, prev_event, recent_event):
+        """ recent event must not be none """
         self.name = recent_event.name
         if prev_event is None:
-          self.prev_exped_name = '-'
+            self.prev_exped_name = '-'
             self.prev_time = '-' 
         else:
-          if prev_event.exped is None:
-            self.prev_exped_name = '-'
+            if prev_event.exped is None:
+                self.prev_exped_name = '-'
           else:
-            self.prev_exped_name = prev_event.exped.name
+              self.prev_exped_name = prev_event.exped.name
             self.prev_time = prev_event.inserted_time
         self.recent_exped_name = recent_event.exped.name if recent_event.exped is not None else '-'
         self.recent_time = recent_event.inserted_time 
 
     def __repr__(self):
-      return ('%s, %s, %s, %s, %s' %(self.name, self.prev_exped_name, self.recent_exped_name, self.prev_time, self.recent_time)).encode('utf8')
+        return ('%s, %s, %s, %s, %s' %(self.name, self.prev_exped_name, self.recent_exped_name, self.prev_time, self.recent_time)).encode('utf8')
 
 def recent_event(request):
-  recent_event_list = Player.objects.all().order_by('-inserted_time')[:100]  
+    recent_event_list = Player.objects.all().order_by('-inserted_time')[:100]  
     mig_history_list = []
     for r_event in recent_event_list:
-      prev_event = None
+        prev_event = None
         player_event_list = Player.objects.filter(name=r_event.name).order_by('-inserted_time')
         for p_event in player_event_list:
-          #print r_event.name, p_event.inserted_time, r_event.inserted_time
+            #print r_event.name, p_event.inserted_time, r_event.inserted_time
             if p_event.inserted_time < r_event.inserted_time:
-              #print 'pass'
+                #print 'pass'
                 prev_event = p_event
                 break;
         mig_history_list.append(MigHistory(prev_event, r_event))
@@ -59,12 +59,12 @@ def recent_event(request):
     return render_to_response('inout_manager/player_history.dj.html', context)
 
 def player_event(request, player_name):
-  player_event_list = Player.objects.filter(name=player_name).order_by('inserted_time')
+    player_event_list = Player.objects.filter(name=player_name).order_by('inserted_time')
 
     mig_history_list = [] 
     prev_event = None
     for event in player_event_list:
-      mig_history_list.append(MigHistory(prev_event, event))
+        mig_history_list.append(MigHistory(prev_event, event))
         prev_event = event
 
     context = {'mig_history_list':mig_history_list}
